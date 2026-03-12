9bc636e729327ec5a4afc8ffe6ce39f6ba0a464b
Someswar <someswarasai.p21@iiits.in>
2026-03-12 23:06:01 +0530

    Day 4: React State and Hooks practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import Header from "./components/Header";
 import ProductCard from "./components/ProductCard";
 import Footer from "./components/Footer";
+import UserForm from "./components/UserForm";
 
 function App() {
   return (
     <div>
 
-      <Header />
+      <UserForm />
 
+      {/* <Header />
       <ProductCard
         title="React Basics"
         description="Understanding components and props"
       />
-
       <ProductCard
         title="Component Architecture"
         description="Building reusable UI components"
       />
-
-      <Footer />
+      <Footer /> */}
 
     </div>
   );
