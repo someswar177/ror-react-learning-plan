40182c0c16b2167dddaf08c669495b757d979ddd
someswar177 <someswarasai.p21@iiits.in>
2026-03-16 13:43:50 +0530

    mocking handleclick function when button clicked

--- src/components/TodoList.test.jsx
+++ src/components/TodoList.test.jsx
@@ -3,7 +3,7 @@ import TodoList from './TodoList';
 import React from 'react';
 import userEvent from '@testing-library/user-event';
 
-test('renders todo input field', ()=>{
+test('renders todo input field', () => {
     render(<TodoList />);
 
     const input = screen.getByPlaceholderText('Add a todo');
@@ -11,7 +11,7 @@ test('renders todo input field', ()=>{
     expect(input).toBeInTheDocument();
 })
 
-test('add a todo item', async()=>{
+test('add a todo item', async () => {
     render(<TodoList />);
 
     const input = screen.getByPlaceholderText('Add a todo');
@@ -21,13 +21,23 @@ test('add a todo item', async()=>{
     expect(input).toHaveValue('Learn React Testing Library');
 })
 
-test('adds todo item to list when button clicked', async()=>{
+test('adds todo item to list when button clicked', async () => {
     render(<TodoList />);
     const input = screen.getByPlaceholderText('Add a todo');
-    const button = screen.getByRole('button', {name: /add/i});
+    const button = screen.getByRole('button', { name: /add/i });
 
     await userEvent.type(input, 'Learn React Testing Library');
     await userEvent.click(button);
 
     expect(screen.getByText('Learn React Testing Library')).toBeInTheDocument();
+})
+
+test('calls callback when button clicked', async () => {
+    const handleClick = jest.fn();
+
+    render(<button onClick={handleClick}>Click</button>);
+
+    await userEvent.click(screen.getByText("Click"));
+
+    expect(handleClick).toHaveBeenCalled();
 })
