28e83823c4520283d531ceadb5492d6711576aad
someswar177 <someswarasai.p21@iiits.in>
2026-03-16 13:37:38 +0530

    green: interaction testing, tested whether list is showing in ui after after adding

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,6 +3,14 @@ import React from "react";
 
 function TodoList() {
   const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function addTodo() {
+    if (!todo) return;
+
+    setTodos([...todos, todo]);
+    setTodo("");
+  }
 
   return (
     <div>
@@ -11,6 +19,14 @@ function TodoList() {
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
       />
+
+      <button onClick={addTodo}>Add</button>
+
+      <ul>
+        {todos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/components/TodoList.test.jsx
+++ src/components/TodoList.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import TodoList from './TodoList';
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 
 test('renders todo input field', ()=>{
     render(<TodoList />);
@@ -8,4 +9,25 @@ test('renders todo input field', ()=>{
     const input = screen.getByPlaceholderText('Add a todo');
 
     expect(input).toBeInTheDocument();
+})
+
+test('add a todo item', async()=>{
+    render(<TodoList />);
+
+    const input = screen.getByPlaceholderText('Add a todo');
+
+    await userEvent.type(input, 'Learn React Testing Library');
+
+    expect(input).toHaveValue('Learn React Testing Library');
+})
+
+test('adds todo item to list when button clicked', async()=>{
+    render(<TodoList />);
+    const input = screen.getByPlaceholderText('Add a todo');
+    const button = screen.getByRole('button', {name: /add/i});
+
+    await userEvent.type(input, 'Learn React Testing Library');
+    await userEvent.click(button);
+
+    expect(screen.getByText('Learn React Testing Library')).toBeInTheDocument();
 })
