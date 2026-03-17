b7df183f8f86d4e5fd8c404cc508e02e96ac8658
someswar177 <someswarasai.p21@iiits.in>
2026-03-17 13:56:46 +0530

    Day 6: refactor todo app using useReducer and Context API

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,19 +1,30 @@
 import { Button, HStack, Input } from '@chakra-ui/react'
+import { useTodoContext } from './TodoContext';
 import React from 'react'
 
-const TodoForm = ({onAddTodo}) => {
+const TodoForm = () => {
     const [newTodo,setNewTodo] = React.useState("");
+    const { dispatch } = useTodoContext();
     
     function handleSubmit(e){
         e.preventDefault();
         if(newTodo.trim() === "") return;
-        onAddTodo(newTodo);
+        dispatch({
+            type: "ADD_TODO", 
+            payload: newTodo
+        });
         setNewTodo("");
     }
   return (
     <form onSubmit={(e)=>handleSubmit(e)}>
         <HStack my={2} spacing={{ base: 2, md: 4 }}>
-            <Input placeholder='Add a new todo' value={newTodo} onChange={(e)=>setNewTodo(e.target.value)} flex={1} />
+            <Input 
+            placeholder='Add a new todo' 
+            value={newTodo} 
+            onChange={(e)=>setNewTodo(e.target.value)} 
+            flex={1} 
+            />
+            
             <Button type='submit' colorScheme='teal' size={{ base: "sm", md: "md" }}>Add</Button>
         </HStack>
     </form>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,16 +2,15 @@ import React,{useState} from 'react'
 import { Box } from "@chakra-ui/react"
 import TodoForm from './TodoForm';
 import TodoCard from './TodoCard';
+import { useTodoContext } from './TodoContext';
 
 const TodoList = () => {
-    const [todo,setTodo] = useState([]);
-    function addTodo(newTodo){
-        setTodo([...todo,newTodo])
-    }
+    const { state } = useTodoContext();
+    const { todos } = state;
     return (
         <Box maxW={{ base: "100%", md: "600px" }} w="full">
-            <TodoForm onAddTodo={addTodo} />
-            {todo.slice().reverse().map((item,index)=>{
+            <TodoForm />
+            {todos.map((item,index)=>{
                 return <TodoCard key={index} todo={item} />
             })}                        
         </Box>
