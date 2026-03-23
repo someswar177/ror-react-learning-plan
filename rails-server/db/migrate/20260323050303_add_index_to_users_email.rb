87c30eb10f309246c0b8b8413ec0ef13e439a6cf
someswar177 <someswarasai.p21@iiits.in>
2026-03-23 10:44:05 +0530

    added safety measure(disable_ddl_transaction! and algorithm:concurrently) to latest migration file which follows strong migrations rules and now migration working without any errors

--- rails-server/db/migrate/20260323050303_add_index_to_users_email.rb
+++ rails-server/db/migrate/20260323050303_add_index_to_users_email.rb
@@ -1,5 +1,11 @@
 class AddIndexToUsersEmail < ActiveRecord::Migration[8.1]
+  disable_ddl_transaction!
   def change
-    add_index :users, :email
+    add_index :users, :email, algorithm: :concurrently
   end
+
+  # This below code means -> telling Rails: "I know this is dangerous. Do it anyway."
+  # safety_assured do
+  #   add_index :users, :email
+  # end
 end
--- rails-server/db/schema.rb
+++ rails-server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_19_163235) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_23_050303) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -38,6 +38,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_19_163235) do
     t.string "email"
     t.string "name"
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email"
   end
 
   add_foreign_key "comments", "posts"
