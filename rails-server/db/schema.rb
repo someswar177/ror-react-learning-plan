d6472cce1a9eb2535430f2fcbe8d7a10fe5eb6df
someswar177 <someswarasai.p21@iiits.in>
2026-03-24 08:47:34 +0530

    initiated models with proper associations

--- rails-server/db/schema.rb
+++ rails-server/db/schema.rb
@@ -10,27 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_23_050303) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_24_023859) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
-  create_table "comments", force: :cascade do |t|
-    t.text "body"
+  create_table "cart_items", force: :cascade do |t|
+    t.bigint "cart_id", null: false
     t.datetime "created_at", null: false
-    t.bigint "post_id", null: false
+    t.bigint "product_id", null: false
+    t.integer "quantity"
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+    t.index ["product_id"], name: "index_cart_items_on_product_id"
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.text "body"
+  create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
-    t.string "title"
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.string "name"
+    t.float "price"
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,10 +44,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_23_050303) do
     t.string "email"
     t.string "name"
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email"
   end
 
-  add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
-  add_foreign_key "posts", "users"
+  add_foreign_key "cart_items", "carts"
+  add_foreign_key "cart_items", "products"
+  add_foreign_key "carts", "users"
 end
