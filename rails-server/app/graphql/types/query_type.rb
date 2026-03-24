91b20a5c685d88ca1381ade125e8d1d9ab156242
someswar177 <someswarasai.p21@iiits.in>
2026-03-24 14:17:19 +0530

    GREEN: defined product related fields in query type file to pass the test

--- rails-server/app/graphql/types/query_type.rb
+++ rails-server/app/graphql/types/query_type.rb
@@ -14,6 +14,18 @@ module Types
       User.find_by(id:id)
     end
 
+    field :products, [Types::ProductType],null: false
+    def products
+      Product.all
+    end
+
+    field :product, Types::ProductType,null: true do
+      argument :id, ID, required: true
+    end
+    def product(id:)
+      Product.find_by(id:id)
+    end
+
     field :node, Types::NodeType, null: true, description: "Fetches an object given its ID." do
       argument :id, ID, required: true, description: "ID of the object."
     end
