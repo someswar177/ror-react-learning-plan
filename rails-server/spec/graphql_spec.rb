f093429782668ea3de24be297f4efd6e7aab2306
someswar177 <someswarasai.p21@iiits.in>
2026-03-24 12:54:03 +0530

    RED: not defined user field in query_type file

--- rails-server/spec/graphql_spec.rb
+++ rails-server/spec/graphql_spec.rb
@@ -19,5 +19,22 @@ RSpec.describe "GraphQL API", type: :request do
           expect(json["data"]["users"][0]["name"]).to eq("somu")
           expect(json["data"]["users"][0]).to eq({"name"=>"somu","email"=>"somu@example.com"})
         end
+
+        it "returns user" do
+          user = User.create(name:"somu",email:"somu@example.com")
+          post "/graphql", params:{
+            query:"{
+              user(id:#{user.id}){
+                name
+                email
+              }
+            }"
+          }
+
+          json = JSON.parse(response.body)
+
+          expect(json["data"]["user"]["name"]).to eq("somu")
+        end
+
   end  
 end
