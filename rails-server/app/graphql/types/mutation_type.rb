1d2b4f65c076ffbac0e9e8cb3c493e1f34ce6312
someswar177 <someswarasai.p21@iiits.in>
2026-03-24 09:24:29 +0530

    created types and mutations(just initiation) using rails g cmds

--- rails-server/app/graphql/types/mutation_type.rb
+++ rails-server/app/graphql/types/mutation_type.rb
@@ -2,6 +2,18 @@
 
 module Types
   class MutationType < Types::BaseObject
+    field :delete_cart_item, mutation: Mutations::DeleteCartItem
+    field :delete_cart, mutation: Mutations::DeleteCart
+    field :delete_product, mutation: Mutations::DeleteProduct
+    field :update_cart_item, mutation: Mutations::UpdateCartItem
+    field :update_cart, mutation: Mutations::UpdateCart
+    field :update_product, mutation: Mutations::UpdateProduct
+    field :create_cart_item, mutation: Mutations::CreateCartItem
+    field :create_cart, mutation: Mutations::CreateCart
+    field :create_product, mutation: Mutations::CreateProduct
+    field :delete_user, mutation: Mutations::DeleteUser
+    field :update_user, mutation: Mutations::UpdateUser
+    field :create_user, mutation: Mutations::CreateUser
     # TODO: remove me
     field :test_field, String, null: false,
       description: "An example field added by the generator"
