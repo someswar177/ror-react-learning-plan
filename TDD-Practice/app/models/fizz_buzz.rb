a4a723cbc29189f07afe281f52f0c3d9561766fb
someswar177 <someswarasai.p21@iiits.in>
2026-04-07 20:57:24 +0530

    red: add passing test for mutiples of 3 and 5 using generalization formula

--- TDD-Practice/app/models/fizz_buzz.rb
+++ TDD-Practice/app/models/fizz_buzz.rb
@@ -1,6 +1,6 @@
 class FizzBuzz
   def calculate(number)
-    return "fizz" if number == 3
-    return "buzz" if number == 5
+    return "fizz" if number % 3 == 0
+    return "buzz" if number % 5 == 0
   end
 end
