62c1117de30ac98080bd630dbbfabcebe27d9ffb
someswar177 <someswarasai.p21@iiits.in>
2026-04-08 14:08:24 +0530

    green: passing test for returning number itself for single digit number input

--- TDD-Practice/app/models/string_calculator.rb
+++ TDD-Practice/app/models/string_calculator.rb
@@ -1,5 +1,6 @@
 class StringCalculator
     def add(input)
         return 0 if input.empty?
+        return input.to_i if input.length == 1
     end
 end
--- TDD-Practice/spec/string_calculator_spec.rb
+++ TDD-Practice/spec/string_calculator_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe "StringCalculator" do
     calculator = StringCalculator.new
     expect(calculator.add("")).to eq(0)
   end
+
+  it "returns the number itself when single digit number is given" do
+    calculator = StringCalculator.new
+    expect(calculator.add("1")).to eq(1)
+  end
 end
