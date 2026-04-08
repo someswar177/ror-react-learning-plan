128528002f00d5ee3185007a6093dea1001f7d41
someswar177 <someswarasai.p21@iiits.in>
2026-04-08 18:22:09 +0530

    green: passing test by throwing an exception for negative number in input

--- TDD-Practice/app/models/string_calculator.rb
+++ TDD-Practice/app/models/string_calculator.rb
@@ -10,8 +10,15 @@ class StringCalculator
         end
         numbers = input.split(delimiter)
         sum = 0
+        negative = nil
         for number in numbers
+            if number.to_i < 0
+                negative = number.to_i
+            end
             sum += number.to_i
+        end
+        if negative != nil
+            raise "negatives not allowed: #{negative}"
         end
         return sum
     end
--- TDD-Practice/spec/string_calculator_spec.rb
+++ TDD-Practice/spec/string_calculator_spec.rb
@@ -35,4 +35,9 @@ RSpec.describe "StringCalculator" do
       calculator = StringCalculator.new
       expect(calculator.add("//;\n1;2")).to eq(3)
   end
+
+  it "raises an exception for a single negative number" do
+    calculator = StringCalculator.new
+    expect { calculator.add("1,-2,3") }.to raise_error("negatives not allowed: -2")
+  end
 end
