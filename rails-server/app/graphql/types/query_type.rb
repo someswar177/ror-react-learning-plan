5efb0e8103e651b6adbc8c0740c572bbd8ebb601
someswar177 <someswarasai.p21@iiits.in>
2026-04-09 12:36:47 +0530

    rails-server setup with rspec and grpahql installs

--- rails-server/app/graphql/types/query_type.rb
+++ rails-server/app/graphql/types/query_type.rb
@@ -2,58 +2,6 @@
 
 module Types
   class QueryType < Types::BaseObject
-    field :users, [Types::UserType],null: false
-    def users
-      User.all
-    end
-
-    field :user, Types::UserType,null: true do
-      argument :id, ID, required: true
-    end
-    def user(id:)
-      User.find_by(id:id)
-    end
-
-    field :user_by_email, Types::UserType, null: true do
-      argument :email, String, required: true
-    end
-    def user_by_email(email:)
-      puts email
-      User.find_by(email: email)
-    end
-
-    field :products, [Types::ProductType], null: false do
-      argument :search, String, required: false
-      argument :max_price, Float, required: false
-    end
-    def products(search: nil, max_price: nil)
-      products = Product.all
-
-      if search.present?
-        products = products.where("name ILIKE ?", "%#{search}%")
-      end
-
-      if max_price.present?
-        products = products.where("price <= ?", max_price)
-      end
-
-      products
-    end
-
-    field :product, Types::ProductType,null: true do
-      argument :id, ID, required: true
-    end
-    def product(id:)
-      Product.find_by(id:id)
-    end
-
-    field :cart, Types::CartType, null: true do
-      argument :user_id, ID, required: true
-    end
-    def cart(user_id:)
-      Cart.find_by(user_id: user_id)
-    end
-
     field :node, Types::NodeType, null: true, description: "Fetches an object given its ID." do
       argument :id, ID, required: true, description: "ID of the object."
     end
